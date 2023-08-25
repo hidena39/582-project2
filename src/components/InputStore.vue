4febdf7a8b34cc10462251969e5538eda2dd70ab
DIDI-LAPTOP\hiden <hidenadeluca@gmail.com>
2023-08-25 16:08:05 -0400

    minor fix

--- src/components/InputStore.vue
+++ src/components/InputStore.vue
@@ -31,6 +31,7 @@ export default {
       let storename = document.querySelector("#storename").value;
       const formData = { storename };
       console.log(storename);
+      //adding store name to db
       fetch("https://reimagined-eureka-7qvqxw66r4w3pww9-3000.app.github.dev/", {
         method: "post",
         body: JSON.stringify(formData),
