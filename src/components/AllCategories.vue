7a956c49bac02fa6e922e0dbcb64c1619f174fee
VANIERCOLLEGE\2295439 <2295439@edu.vaniercollege.qc.ca>
2023-08-29 15:52:52 -0400

    able to show categories in AllCategories

--- src/components/AllCategories.vue
+++ src/components/AllCategories.vue
@@ -1,13 +1,25 @@
 <template>
   <h1>AllCategories</h1>
-  <OneCategory />
+  <div class="storeCardList">
+    <OneCategory
+      v-for="category in categories"
+      :key="category"
+      :oneCategory="category"
+    />
+    <!-- <OneCategory v-for="item in itemToList" :key="item" /> -->
+  </div>
 </template>
 
 <script>
+import { useListStore } from "@/store/storelist";
 import OneCategory from "@/components/OneCategory.vue";
 
 export default {
   name: "AllCategories",
+  setup() {
+    const liststore = useListStore();
+    return { liststore };
+  },
   // data() {
   //     return {
 
@@ -19,6 +31,21 @@ export default {
   // methods: {
 
   // },
+  computed: {
+    categories() {
+      const storeName = this.$route.params.storename;
+      const allcategories = [];
+      for (const store of this.liststore.stores) {
+        //Ehnr categories are empty, it gives an error. So making it check if there are categories
+        if (store.storename === storeName && store.categories) {
+          for (let i = 0; i < store.categories.length; i++) {
+            allcategories.push(store.categories[i].category);
+          }
+        }
+      }
+      return allcategories;
+    },
+  },
 };
 </script>
 <!-- 
--- src/components/OneCategory.vue
+++ src/components/OneCategory.vue
@@ -1,15 +1,36 @@
 <template>
-  <h1>OneCategory</h1>
-  <p>{{ liststore.stores }}</p>
+  <h1>{{ oneCategory }}</h1>
+  <div></div>
 </template>
 
 <script>
 import { useListStore } from "@/store/storelist";
 export default {
   name: "OneCategory",
+  props: {
+    oneCategory: String,
+  },
   setup() {
     const liststore = useListStore();
     return { liststore };
+  },
+  computed: {
+    Items() {
+      const storeName = this.$route.params.storename;
+      const allItems = [];
+      for (const store of this.liststore.stores) {
+        //Ehnr categories are empty, it gives an error. So making it check if there are categories
+        if (store.storename === storeName && store.categories) {
+          if (categories.cateory === this.oneCategory) {
+            for (let i = 0; i < store.categories.length; i++) {
+              allItems.push(store.categories[i].items);
+            }
+          }
+        }
+      }
+      console.log("allItems", allItems);
+      return allItems;
+    },
   },
   // data() {
   //     return {
@@ -22,21 +43,6 @@ export default {
   // methods: {
 
   // },
-  computed: {
-    categories() {
-      const storeName = this.$route.params.storename;
-      const allcategories = [];
-      for (const store of this.liststore.stores) {
-        //Ehnr categories are empty, it gives an error. So making it check if there are categories
-        if (store.storename === storeName && store.categories) {
-          for (let i = 0; i < store.categories.length; i++) {
-            allcategories.push(store.categories[i].category);
-          }
-        }
-      }
-      return allcategories;
-    },
-  },
 };
 </script>
 
