1319d17fcb9ade1113f134ce26fe4bd892ddc090
DIDI-LAPTOP\hiden <hidenadeluca@gmail.com>
2023-09-01 11:39:48 -0400

    each items can be marked as purchased with time

--- src/components/OneItem.vue
+++ src/components/OneItem.vue
@@ -2,8 +2,8 @@
   <li>
     <div class="oneItem" @click="changeStatus">{{ oneItem }}</div>
     <div><button class="deleteButton" @click="deleteItem">delete</button></div>
-    <div class="purchased"></div>
-    {{ purchaseStatus }}
+    <div class="purchased" v-if="purchaseStatus" @click="changeStatus"></div>
+    <div class="purchasedTime" v-if="purchaseStatus">{{ purchasedTime }}</div>
   </li>
 </template>
 
@@ -37,6 +37,24 @@ export default {
         }
       }
       return purchased;
+    },
+    purchasedTime() {
+      const storeName = this.$route.params.storename;
+      let clickeTime = "";
+      for (const store of this.liststore.stores) {
+        if (store.storename === storeName) {
+          for (const category of store.categories) {
+            if (category.category === this.oneCategory) {
+              for (const item of category.items) {
+                if (item.item === this.oneItem) {
+                  clickeTime = item.time;
+                }
+              }
+            }
+          }
+        }
+      }
+      return clickeTime;
     },
   },
   methods: {
@@ -73,8 +91,42 @@ export default {
         });
     },
     changeStatus() {
-      console.log("hi");
       console.log(this.purchaseStatus);
+      console.log("submitted");
+      const storename = this.$route.params.storename;
+      const purchaseStatus = this.purchaseStatus;
+      const oneCategory = this.oneCategory;
+      const oneItem = this.oneItem;
+      const changedStatus = !purchaseStatus;
+      fetch(
+        `https://reimagined-eureka-7qvqxw66r4w3pww9-3000.app.github.dev/each-store/${storename}/changestatus`,
+        {
+          method: "post",
+          body: JSON.stringify({
+            category: oneCategory,
+            item: oneItem,
+            purchased: changedStatus,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+          },
+        }
+      )
+        .then((res) => {
+          return res.text();
+        })
+        .then((data) => {
+          console.log(data);
+          fetch(
+            `https://reimagined-eureka-7qvqxw66r4w3pww9-3000.app.github.dev/`
+          )
+            .then((response) => response.json())
+            .then((json) => {
+              console.log(json);
+              this.liststore.stores = json;
+            });
+        });
     },
   },
 };
@@ -96,6 +148,9 @@ li {
   background-color: rgb(84, 170, 204, 0.5);
   top: 0;
   right: 0;
+  cursor: pointer;
+}
+.displayNone {
   display: none;
 }
 </style>
